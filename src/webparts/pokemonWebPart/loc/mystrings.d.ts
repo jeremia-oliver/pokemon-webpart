442a177863315610db8859a76ec1a037bfafda5d
Jeremi <23479392+jeremia-oliver@users.noreply.github.com>
2024-06-23 18:39:37 +0700

    Initial Commit

---
+++ src/webparts/pokemonWebPart/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IPokemonWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'PokemonWebPartWebPartStrings' {
+  const strings: IPokemonWebPartWebPartStrings;
+  export = strings;
+}
